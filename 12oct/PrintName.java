0f960169f6a35518e24db3d05440efd42a49517f
Shuruti Singhal <110531820+Shuruti1234@users.noreply.github.com>
2022-10-14 11:10:49 +0530

    Add files via upload

---
+++ 12oct/PrintName.java
@@ -0,0 +1,28 @@
+
+ class PrintName
+ {
+	 public static final void main(String args[])
+	 {
+		 System.out.println("First Name "+ args[0]);
+		 
+		 System.out.println("Last Name"+ args[1]);
+
+	 }
+ }
+ 
+
+//1. If we omit public, static : Excecution error
+//2. If we omit void :Compilation error
+// 3. we can change the position of public and static 
+// 4. we cannot change the position of void: Compilation error
+//5. omit [] of args: Execution error
+//6. compilation is not case sensitive
+//7. execution is case sensitive
+//8 main method is always final ,, so we can write or omit also
+
+
+Name: Shuruti Singhal
+Age: 30
+Course: Java
+Score: A+
+
