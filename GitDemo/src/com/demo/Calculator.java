4cbfcd2902015548426d22265450e94908ed9857
ShurutiSingha1234 <shruti.17july@gmail.com>
2022-10-25 12:22:27 +0530

    Calculator using packages

---
+++ GitDemo/src/com/demo/Calculator.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Calculator {
+
+	public static float addition(float num1, float num2) {
+		return num1 + num2;
+	}
+
+	public static float subtraction(float num1, float num2) {
+		return num1 - num2;
+	}
+
+	public static float multiplication(float num1, float num2) {
+		return num1 * num2;
+	}
+
+	public static float division(float num1, float num2) {
+		return num1 / num2;
+	}
+}
---
+++ GitDemo/src/com/test/CalcTest.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import java.util.Scanner;
+
+import com.demo.Calculator;
+
+public class CalcTest {
+
+	static void menu() {
+		System.out.println("1.Addition");
+		System.out.println("2.Subtraction");
+		System.out.println("3.Multiplication");
+		System.out.println("4. Division");
+		System.out.println("5. Exit");
+		System.out.println("Enter choice :");
+
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int choice;
+		float number1, number2, result = 0.0f;
+
+		System.out.println("Enter first number:");
+		number1 = sc.nextFloat();
+
+		System.out.println("Enter second number:");
+		number2 = sc.nextFloat();
+
+		menu();
+
+		choice = sc.nextInt();
+
+		switch (choice) {
+
+		case 1:
+			result = Calculator.addition(number1, number2);
+			break;
+		case 2:
+			result = Calculator.subtraction(number1, number2);
+			break;
+		case 3:
+			result = Calculator.multiplication(number1, number2);
+			break;
+		case 4:
+			result = Calculator.division(number1, number2);
+			break;
+		case 5:
+			System.exit(0);
+		default:
+			System.out.println("Invalid input.........");
+
+		}
+
+		System.out.println("The Result is:" + result);
+
+	}
+
+}
